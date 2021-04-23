80dca0ac75ca011c77ce63ce66421a14147a3b14
adi8900 <adrianszymanski242@gmail.com>
2021-04-23 21:30:25 +0200

    violet: Support for livedisplay
    This reverts commit ee90bb0b296ccdd4a68b874d12c253f31d6e28c2.
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -294,6 +294,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sm6150
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_sm6150
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
