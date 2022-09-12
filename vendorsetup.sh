bff1e3d288f842197fe858710e61af8e0808d5b6
rdx420 <padraramesh420@gmail.com>
2022-09-12 13:57:40 +0530

    violet: Track from thirteen-caf-sm8150 branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,5 +7,5 @@ rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media
 git clone https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
-git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b thirteen-caf-sm8150  hardware/qcom-caf/sm8150/audio
 git clone https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
