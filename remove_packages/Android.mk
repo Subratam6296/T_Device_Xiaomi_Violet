7122dbe2c7c52bd6d788871db20caa1a95fe76c2
rdx420 <padraramesh420@gmail.com>
2022-10-19 16:46:18 +0530

    violet: Add some packages to RemovePackages list
    Signed-off-by: rdx420 <padraramesh420@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail Drive ConnMO DCMO YouTube
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail Drive ConnMO DCMO YouTube MusicFX AudioFX
 LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
 LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap talkback PixelLiveWallpaperPrebuilt
 LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib AndroidAutoStubPrebuilt
