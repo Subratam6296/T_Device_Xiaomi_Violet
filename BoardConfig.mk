3f03649b4eddf5baa31ef154189c43faa074eb67
adi8900 <adrianszymanski242@gmail.com>
2022-10-23 09:57:07 +0530

    violet: Switch to legacy sepolicy_vndr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_violet
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
